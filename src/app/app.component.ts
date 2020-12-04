67646e2e368b0ffb52c30d55f72a98ba0cf7c8db
Asafe Kauffmann <asafe.kauffmann@gmail.com>
2020-12-04 15:28:13 -0300

    Angular first update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,20 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  
+    photos = [
+      {
+        url:
+            "https://cdn.pixabay.com/photo/2016/09/10/19/56/lions-1660044_1280.jpg", 
+            description: "Leao"
+      },
+
+      {
+        url: "https://statig0.akamaized.net/bancodeimagens/dg/qi/e6/dgqie66ooeyri4whdebd9oqh9.jpg",  
+        description: "Leoa"
+      }
+    ];
+
 }
